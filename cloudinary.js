72280607e437af9cf0e8228f8509dee60c40a3aa
PavithraSai849 <lmsplatform46@gmail.com>
2024-11-02 20:29:31 +0530

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -1,31 +1,37 @@
 const cloudinary = require("cloudinary").v2;
 
-//configure with env data
+// Configure with environment variables
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadMediaToCloudinary = async (filePath) => {
-  try {
-    const result = await cloudinary.uploader.upload(filePath, {
-      resource_type: "auto",
-    });
+// Function to upload media to Cloudinary
+const uploadMediaToCloudinary = async (buffer) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { resource_type: "auto" },
+      (error, result) => {
+        if (error) {
+          console.error(error);
+          return reject(new Error("Error uploading to Cloudinary"));
+        }
+        resolve(result);
+      }
+    );
 
-    return result;
-  } catch (error) {
-    console.log(error);
-    throw new Error("Error uploading to cloudinary");
-  }
+    uploadStream.end(buffer);
+  });
 };
 
+// Function to delete media from Cloudinary
 const deleteMediaFromCloudinary = async (publicId) => {
   try {
     await cloudinary.uploader.destroy(publicId);
   } catch (error) {
-    console.log(error);
-    throw new Error("failed to delete assest from cloudinary");
+    console.error(error);
+    throw new Error("Failed to delete asset from Cloudinary");
   }
 };
 
